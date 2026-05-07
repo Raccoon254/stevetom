1c6f7fabf1aeedd0b6f079c4019ff94c9a036477
Steve Tom <tomsteve187@gmail.com>
2026-05-07 18:38:11 +0300

    Initial commit

---
+++ openx/var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2023-03-13 18:02:54
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
---
+++ openx/var/templates_compiled/%%EC^ECD^ECDFE081%%passthrough.html.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.18, created on 2023-03-13 21:32:19
+         compiled from passthrough.html */ ?>
+<?php echo $this->_tpl_vars['content']; ?>
