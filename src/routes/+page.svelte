c837385e25fb16f4a54ec4ec5e38ee3dfbc13ee7
Raccoon254 <tomsteve187@gmail.com>
2026-05-07 18:38:12 +0300

    SEO fix kentom

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,23 +62,6 @@
 	})
 </script>
 
-<svelte:head>
-	<title>Steve Tom | Applied Computer Science Student & Self-Taught Developer</title>
-	<meta
-		name="description"
-		content="Steve Tom alias Raccoon254 - Graduate from Chuka University, specializing in Applied Computer Science. Experienced in YouTube, online courses, computer engineering, networking, Python, PHP, JavaScript, and Kotlin for Android Studio."
-	/>
-	<link
-		rel="stylesheet"
-		type="text/css"
-		href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
-	/>
-	<link
-		rel="stylesheet"
-		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-	/>
-</svelte:head>
-
 <main class="min-h-screen text-white flex flex-col justify-center items-center p-4 md:p-8">
 	<Cursor />
 	<div class="max-w-5xl mt-8 w-full">
